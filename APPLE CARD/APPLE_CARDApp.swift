2a011b815f6e738bb6e2aca1e4b61711c06fcd8b
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2020-12-21 16:29:50 +0530

    Initial Commit

---
+++ APPLE CARD/APPLE_CARDApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPLE_CARDApp.swift
+//  APPLE CARD
+//
+//  Created by SHUBHAM KUMAR on 21/12/20.
+//
+
+import SwiftUI
+
+@main
+struct APPLE_CARDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
